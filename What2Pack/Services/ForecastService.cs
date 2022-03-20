e77691498158e7d56ad76dc9974f663716b9e0af
Chris Butcher <butchchr@gmail.com>
2022-03-20 15:21:46 -0400

    W2P-3 - Added notes to Weatherstack service have a base query in postman to hit historical.  Ran out of time to do more

--- What2Pack/Services/ForecastService.cs
+++ What2Pack/Services/ForecastService.cs
@@ -9,16 +9,24 @@ namespace What2Pack.Api.Services
 
     public class ForecastService : IForecastService
     {
+        private IWeatherstackService WeatherstackService { get; }
         private Serilog.ILogger Log { get; }
+        
 
-        public ForecastService(Serilog.ILogger log)
-        {
+        public ForecastService(IWeatherstackService weatherstackService, Serilog.ILogger log)
+        {   
+            WeatherstackService= weatherstackService;   
             Log = log;
         }
 
+        // This class exists with the assumption that we will be doing something with the forecast object after it comes back from weatherstack
+        // Currently just passing the basic weather back 
         public async Task<ServiceResult<WeatherResponse>> GetWeather(WeatherRequest weatherRequest)
         {
             Log.Verbose("Received request to get weather for {location}", weatherRequest.Location);
+
+            // Call weatherstackService
+
 
             var placeholder = new WeatherResponse
             {
@@ -27,6 +35,8 @@ namespace What2Pack.Api.Services
                 WeatherDescription = "Sunny",
                 Precip =0
             };
+
+
 
             return new ServiceResult<WeatherResponse>().SetValue(placeholder);
         }
--- What2Pack/Services/WeatherstackService.cs
+++ What2Pack/Services/WeatherstackService.cs
@@ -1,8 +1,10 @@
-﻿namespace What2Pack.Api.Services
+﻿using What2Pack.Api.Models;
+
+namespace What2Pack.Api.Services
 {
     public interface IWeatherstackService
     {
-
+        ServiceResult<WeatherResponse> GetHistoricalWeather(WeatherRequest weatherRequest);
     }
 
     public class WeatherstackService : IWeatherstackService
@@ -12,6 +14,25 @@
         public WeatherstackService(Serilog.ILogger log)
         {
             Log = log;
+        }
+
+        public ServiceResult<WeatherResponse> GetHistoricalWeather(WeatherRequest weatherRequest)
+        {
+            // Ran out of time to put together more.  Would use rest sharp to call out the API. 
+            // Get JSON back, access the fields I need and return the object.
+            // There is some business logic to do to take a start date and duration. 
+            // Another endpoint option would be to take a start and end date
+
+
+            var placeholder = new WeatherResponse
+            {
+                Location = "Copper Harbor",
+                Temperature = 2,
+                WeatherDescription = "Sunny",
+                Precip = 0
+            };
+
+            return new ServiceResult<WeatherResponse>().SetValue(placeholder);
         }
     }
 }
